526981e6f58e2c40d495519abc8bd7a3c3a5a344
shubham <nakumsky@gmail.com>
2023-03-15 00:10:34 +0530

    Hey my commit

---
+++ microservice-demo/microservice-demo/twitter-to-kafak-ms/src/test/java/com/microservice/twitter_to_kafka_service/TwitterToKafkaServiceTest.java
@@ -0,0 +1,14 @@
+package com.microservice.twitter_to_kafka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TwitterToKafkaServiceTest {
+
+    @Test
+    public void loadContext(){
+
+    }
+
+}
